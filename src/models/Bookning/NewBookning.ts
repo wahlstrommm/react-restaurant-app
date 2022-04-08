ae5cf503d46ec31a7fdf27f3ed8b124314457a75
ZyrhonDev <93902196+ZyrhonDev@users.noreply.github.com>
2022-04-08 09:29:24 +0200

    fixed errors

--- src/models/Bookning/NewBookning.ts
+++ src/models/Bookning/NewBookning.ts
@@ -1,13 +1,12 @@
 export class NewBookning{
     restaurantId:string
-date:string
-time:string
-numberOfGuests:number
+    date:string
+    time:string
+    numberOfGuests:number
 constructor(restaurantId:string,date:string,time:string,numberOfGuests:number){
     this.restaurantId = restaurantId,
     this.date=date,
     this.time=time,
     this.numberOfGuests=numberOfGuests;
-    
-}
+    }
 }
