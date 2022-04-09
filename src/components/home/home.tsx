a3ae8eff2a445f80193e9e1b2673cce4e98d6106
Malin Johansson <malin.johansson@medieinstitutet.se>
2022-04-09 11:35:10 +0200

    content created

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import drinkPic from "../../assets/glas.svg";
 
 export default function home() {
   return (
-    <div>home</div>
-  )
+    <div>
+      <div>
+        <h1>Zias</h1>
+        <img src={drinkPic} alt="Bild på drink" />
+      </div>
+      <div>
+        <h1>Restaurangen</h1>
+        <p>
+          Hos oss på Restaurang Zias får ni möjlighet att avnjuta karibiska och
+          kubanska maträtter tillagade med utsökta och annorlunda råvaror.
+          Fantaastiska drinkar och ett bra utbud på öl och rom.
+        </p>
+        <span></span>
+      </div>
+    </div>
+  );
 }
