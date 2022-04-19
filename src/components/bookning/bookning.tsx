dc1aa175f0a222c667b6ca61ebeac16c5630c302
ZyrhonDev <93902196+ZyrhonDev@users.noreply.github.com>
2022-04-20 01:09:02 +0200

    Lagt till validering, behövs importera redan bokade ordrar och sortera dem efter valt datum för att få ut lediga tider den dagen, samt styling.

--- src/components/bookning/bookning.tsx
+++ src/components/bookning/bookning.tsx
@@ -2,9 +2,12 @@ import axios from 'axios';
 import React, { ChangeEvent, useState } from 'react'
 import { NewBookning } from '../../models/Bookning/NewBookning';
 import { INewCostumer } from '../../models/Costumer/INewCostumer'
+import './styles.css';
+import { useForm } from 'react-hook-form'
 
 export default function Bookning() {
   let bookingURL = "https://school-restaurant-api.azurewebsites.net/booking/create";
+  const { register, handleSubmit, formState: {errors} } = useForm();
 
   // Skapar en customer för att sen kunna lägga till i bokning
   const [newCustomer, setNewCustomer] = useState<INewCostumer>({
@@ -28,17 +31,23 @@ export default function Bookning() {
     }
   });
 
+
+
   const handleBookingChange = (e: ChangeEvent<HTMLInputElement>) => {
     let name: string = e.target.name;
     setNewBooking({...newBooking, [name]: e.target.value});
-    console.log(newBooking);
   };
 
   const handleCustomerChange = (e: ChangeEvent<HTMLInputElement>) => {
     let name: string = e.target.name;
     setNewCustomer({...newCustomer, [name]: e.target.value});
-    console.log(newCustomer);
   };
+
+  // Behövde skapa en egen handleChange för select eftersom de andra tar in events från Input Element. 
+  const handleTimeChange = (e:ChangeEvent<HTMLSelectElement>) => {
+    let name: string = e.target.name;
+    setNewBooking({...newBooking, [name]: e.target.value})
+  }
 
 
   function sendBooking() {
@@ -57,26 +66,72 @@ export default function Bookning() {
     .catch(error => {console.log(error)})
   }
 
-
+  const onSubmit = handleSubmit((data) => {
+    sendBooking();
+  });
+  
   return (
     <>
-      <form>
-        <label>First name:</label>
-        <input type="text" name="name" value={newCustomer.name} onChange={handleCustomerChange}/>
-        <label>Last name:</label>
-        <input type="text" name="lastname" value={newCustomer.lastname} onChange={handleCustomerChange}/>
-        <label>Email:</label>
-        <input type="text" name="email" value={newCustomer.email} onChange={handleCustomerChange}/>
-        <label>Phone number:</label>
-        <input type="text" name="phone" value={newCustomer.phone} onChange={handleCustomerChange}/>
-        <label>Date of booking:</label>
-        <input type="date" name="date" value={newBooking.date} onChange={handleBookingChange}/>
-        <label>Time to reserve table:</label>
-        <input type="time" name="time" value={newBooking.time} onChange={handleBookingChange}/>
-        <label>Number of guests:</label>
-        <input type="number" name="numberOfGuests"  value={newBooking.numberOfGuests} onChange={handleBookingChange}/>
+      <form onSubmit={onSubmit}>
+        <div>
+          <label>First name:</label>
+          <input 
+          {...register("name", {required: true})}
+          type="text" 
+          name="name" 
+          value={newCustomer.name} 
+          onChange={handleCustomerChange}
+          />
+          {errors.name && "Namn är obligatoriskt"}
+         </div>
+         <div>
+          <label>Last name:</label>
+          <input 
+          {...register("lastname", {required: true})}
+          type="text" name="lastname" value={newCustomer.lastname} onChange={handleCustomerChange}/>
+          {errors.lastname && "Efternamn är obligatoriskt"}
+        </div>
+        <div>
+          <label>Email:</label>
+          <input 
+          {...register("email", {required: true})}
+          type="text" name="email" value={newCustomer.email} onChange={handleCustomerChange}/>
+          {errors.email && "Email är obligatoriskt"}
+        </div>
+        <div>
+          <label>Phone number:</label>
+          <input 
+          {...register("phone", {required: true})}
+          type="text" name="phone" value={newCustomer.phone} onChange={handleCustomerChange}/>
+          {errors.phone && "Telefonnummer är obligatoriskt"}
+        </div>
+        <div>
+          <label>Date of booking:</label>
+          <input 
+          {...register("date", {required: true})}
+          type="date" name="date" value={newBooking.date} onChange={handleBookingChange}/>
+          {errors.date && "Datum är obligatoriskt"}
+        </div>
+        <div>
+          <label>Number of guests:</label>
+          <input 
+          {...register("numberOfGuests", {required: true})}
+          type="number" name="numberOfGuests"  value={newBooking.numberOfGuests} onChange={handleBookingChange}/>
+          {errors.numberOfGuests && "Antal gäster är obligatoriskt"}
+        </div>
+        <div>
+          <label>Tid: </label>
+          <select 
+          {...register("time-select", {required: true})}
+          name="time" id="timeSelect" onChange={handleTimeChange}>
+            <option value="18:00">18:00</option>
+            <option value="21:00">21:00</option>
+          </select>
+          {errors.time && "Vänligen välj en tidpunkt"}
+        </div>
+        <input type="submit" />
       </form>
-      <button onClick={sendBooking}>Book a table!</button>
+
     </>
   )
 }
